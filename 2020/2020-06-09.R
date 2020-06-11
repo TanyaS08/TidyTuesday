e7a14a695488d68123fb3ac0fcbb8262ca82405a
Tanya Strydom <tanya.strydom@icloud.com>
2020-06-11 10:32:32 +0100

    chnaged to a Sankey type diagram

--- 2020/2020-06-09.R
+++ 2020/2020-06-09.R
@@ -10,16 +10,22 @@
 library(tidyverse)
 library(tidylog)
 library(lubridate)
+library(ggforce)
 devtools::install_github("thebioengineer/tidytuesdayR")
 
 ### >> b) Import data and clean ----
 
-#Get data fro the week
+#Get data for the week
 tuesdata <- tidytuesdayR::tt_load(2020, week = 24)
 
+### 1) Visualising Data ----
+### >> a) Firsts ----
+
 #extract the first df
-firsts <- tuesdata$firsts %>%
+firsts <- tuesdata$firsts %>% 
   mutate(
+    #calcualte the decade by subtracting the 10th unit and make factor
+    decade = as.factor(year - year %% 10),
     #turn work categories into a factor
     category = as.factor(category),
     #convert gender to factor
@@ -27,33 +33,45 @@ firsts <- tuesdata$firsts %>%
       #for brevity rename to male and female
       ifelse(gender == "African-American Firsts",
              "male",
-             "female")),
-    year = year(as.Date(as.character(year), format = "%Y")))
+             "female"))) %>%
+  #select only target variables
+  select(.,
+         gender,
+         category,
+         decade) %>%
+  #group by 'target' categories
+  group_by(decade,
+           category,
+           gender) %>%
+  #tally the number for each year, gender, category combo
+  summarise(value = n()) %>%
+  #gather into tidy-er format for ggforce plotting
+  gather_set_data(., 1:3)
+  
+
+#Visusalising
+
+p1 <- ggplot(data = firsts, 
+       aes(x, id = id, split = y, value = value)) +
+  geom_parallel_sets(aes(fill = category), 
+                     alpha = 0.5, 
+                     axis.width = 0.4) +
+  geom_parallel_sets_axes(axis.width = 0.4) +
+  geom_parallel_sets_labels(colour = 'white',
+                            angle = 0) +
+  scale_fill_brewer(palette = "Set3")  +
+  theme_void() +
+  theme(legend.position = "bottom",
+        plot.background = element_rect(fill = "grey50"))
+
+ggsave("2020/week_24/African-American_Achievements.png", 
+       p1, 
+       height = 6, width = 11, 
+       units = "in", dpi = 300)
+
+### >> b) Science ----
 
 #extract the science df
 science <- tuesdata$science
-
-### 1) Visualising Data ----
-### >> a) Firsts ----
-
-ggplot(data = firsts %>%
-         #group by 'target' categories
-         group_by(year,
-                  category,
-                  gender) %>%
-         #tally the number for each year, gender, category combo
-         summarise(cumsum = n()) %>%
-         #now only group by category and gender
-         group_by(category,
-                  gender) %>%
-         #calculate cumulative frequency for each category and gender
-         mutate(cumsum = cumsum(cumsum))) +
-  geom_line(aes(x = year,
-                y = cumsum,
-                colour = gender)) +
-  facet_wrap(vars(category)) +
-  ylab("Cummulative number of achievements by African Americans") +
-  xlab("Year") +
-  theme_bw()
 
 # End of script ----
