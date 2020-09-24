c785be1151a762c6d8659e8fce476904d903a63d
Tanya Strydom <tanya.strydom@icloud.com>
2020-09-24 12:48:08 +0200

    🏔 more or less done...

--- 2020/week39_Himalayan/2020-09-22.R
+++ 2020/week39_Himalayan/2020-09-22.R
@@ -11,11 +11,9 @@ library(tidylog)
 library(ggforce)
 library(ggridges)
 library(ggtext)
-library(broom)
+library(extrafont)
 library(here)
 library(showtext)
-library(cowplot)
-library(stringr)
 library(plyr)
 
 #todays prompts
@@ -28,7 +26,7 @@ expeditions <- readr::read_csv('https://raw.githubusercontent.com/rfordatascienc
 peaks <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-09-22/peaks.csv')
 
 
-annapurna_massive <- 
+annapurna_massif <- 
     expeditions %>%
     #remove unclimbed peaks ane erroneous dates
     filter(peak_name %in% c("Annapurna I", "Annapurna II", "Annapurna III", "Annapurna IV", "Annapurna South", 
@@ -45,48 +43,55 @@ annapurna_massive <-
                                             TRUE ~ as.character(termination_reason)))
 
 # Arranging values in a simplified (and aesthetically necessary) order
-termination_reason_values <- annapurna_massive %>% pull(termination_reason) %>% unique()
+termination_reason_values <- annapurna_massif %>% pull(termination_reason) %>% unique()
 termination_reason_values <- 
   termination_reason_values[c(3, 2, 1, 6, 4, 5)]
 
-summary(as.factor(annapurna_massive$termination_reason))
-
-levels(annapurna_massive$termination_reason) <- termination_reason_values[c(3, 2, 1, 6, 4, 5)]
-
-year = annapurna_massive$year/10
-round_any(year,0.5)
-
 exp <- 
-annapurna_massive %>%
+annapurna_massif %>%
   select(expedition_id, peak_id, peak_name, year, termination_reason) %>% 
   group_by(peak_name) %>%
-  mutate(year = year/10,
+  mutate(reason = factor(termination_reason, 
+                    levels = termination_reason_values,
+                    labels = termination_reason_values),
+        year = year/10,
         decade = round_any(scale(round_any(year,0.5)),0.05),
-        year_pos = case_when(peak_name == "Tilicho" ~ decade,
-                            peak_name == "Annapurna I" ~ decade + 4,
-                            peak_name == "Annapurna South" ~ decade + 8,
-                            peak_name == "Gangapurna" ~ decade + 12,
-                            peak_name == "Annapurna III" ~ decade + 16,
-                            peak_name == "Annapurna IV" ~ decade + 20,
-                            peak_name == "Annapurna II" ~ decade + 24))  %>% 
+        year_pos = case_when(peak_name == "Tilicho" ~ decade - 1,
+                            peak_name == "Annapurna I" ~ decade + 4.5,
+                            peak_name == "Annapurna South" ~ decade + 9,
+                            peak_name == "Gangapurna" ~ decade + 13.5,
+                            peak_name == "Annapurna III" ~ decade + 18,
+                            peak_name == "Annapurna IV" ~ decade + 24,
+                            peak_name == "Annapurna II" ~ decade + 30))  %>% 
   ungroup()  %>% 
-  arrange(peak_name, year_pos, termination_reason) %>%
+  arrange(year_pos, reason) %>%
   dplyr::mutate(id = row_number()) 
+
+peak_names <- 
+    exp  %>% 
+    group_by(peak_name)  %>% 
+    summarise_at(vars(year_pos), mean) %>%
+    left_join(.,
+              exp  %>% 
+                group_by(peak_name, year_pos)  %>% 
+                tally()  %>% 
+                group_by(peak_name) %>%
+                summarise_at(vars(n), max),
+            by = "peak_name")  %>% 
+    mutate(peak_name = case_when(peak_name == "Tilicho" ~ "Tilicho<br/>7 134m",
+                            peak_name == "Annapurna I" ~ "Annapurna I<br/>8 091m",
+                            peak_name == "Annapurna South" ~ "Annapurna South<br/>7 219m",
+                            peak_name == "Gangapurna" ~ "Gangapurna<br/>7 455m",
+                            peak_name == "Annapurna III" ~ "Annapurna III<br/>7 555m",
+                            peak_name == "Annapurna IV" ~ "Annapurna IV<br/>7 525m",
+                            peak_name == "Annapurna II" ~ "Annapurna II<br/>7 937m"))
 
 # Bar colors
 mtn_colors <- c('#ffffffff','#aec0ceff','#7acbefff',
-                '#225997ff','#003062ff','#6a8550ff','#626262ff')
+                '#225997ff','#6a8550ff','#003062ff','#626262ff')
 
 
-ggplot(exp, 
-       aes(x = year_pos,
-            y = 1,
-            group = id,
-            fill = termination_reason,
-            colour = peak_name)) +
-  geom_bar(stat = 'identity',
-           size = 0.2)
-
+plot<-
 ggplot(exp) + 
   geom_bar(aes(x = year_pos, 
                 group = id, 
@@ -95,29 +100,61 @@ ggplot(exp) +
             color = "#101924ff",
             width = 0.3)  +
   labs(
-    title = "EVEREST EXPEDITIONS",
-    subtitle = "Journeys to the top of the world end for many reasons. This graphic\nlooks at the results of all Everest expeditions since 1921.\nEverest has had a colorful history, but even\nstill most expeditions reach the peak.",
-    caption = "Source: The Himalayan Database  |  Visualization: @charliegallaghr"
+    title = "ANNAPURNA MASSIF",
+    subtitle = "The Annapurna massif is home to several world-class treks and has 30 peaks over 6000m in altitude.\nAnnapurna I is the only peak to reach above 8000m but is also the first of the 8000ers to be climbed\nBelow are 7 of the more prominent peaks of the massive\narranged as if one was looking out over the massif from the south\nEach box represents one expedition and the colour indicates the outcome",
+    caption = "Source: The Himalayan Database  |  Visualization: @TanyaS_08"
   ) + 
   scale_x_continuous(expand = c(0,0)) + 
   scale_y_continuous(name = "Number of Expeditions", expand = c(0,0)) + 
   scale_fill_manual(values = mtn_colors) +
   theme_bw() + 
   theme(
+    text = element_text(family = "Courier New", 
+                        color = grey(0.9)),
+    plot.caption = element_text(family = "Courier New", 
+                        size = 8, color = grey(0.4)),
+    title = element_text(family = "Courier New", size = 30),
+    axis.title.y = element_text(family = "Courier New", size = 10,
+                                color = grey(0.9)),
+    plot.subtitle = element_text(family = "Courier New", 
+                                 size = 12, margin = margin(0,0,20,0)),
     legend.title = element_blank(),
+    legend.text = element_text(family = "Courier New", size = 11,
+                                color = grey(0.9)),
     legend.key = element_rect(fill = NA, color = NA),
     legend.key.size = unit(10, "pt"),
     legend.key.width = unit(40, "pt"),
-    legend.position = c(0.24, 0.92),
+    legend.position = c(0.66, 0.87),
+    legend.background = element_rect(fill = NA),
     panel.grid = element_blank(),
+    panel.background = element_rect(fill = "#101924ff"),
     panel.grid.major.y = element_line(color = "#252f39ff"),
     plot.margin = margin(20, 40, 5, 40),
-    plot.background = element_rect(color = NA),
+    plot.background = element_rect(fill = "#101924ff"),
     plot.title.position = "panel",
     axis.title.x = element_blank(),
-    axis.text.x = element_text(color = grey(0.4)),
+    axis.text.x = element_blank(),
     axis.text.y = element_text(color = grey(0.4)),
     axis.ticks.x = element_blank(),
     axis.ticks.y = element_blank()
-  )
+  ) +
+  geom_richtext(data = peak_names,
+        aes(y= n + 5,
+            x = year_pos,
+            label = peak_name),
+            angle = 0,
+            vjust = 1,
+            family = "Courier New") +
+  annotate(
+        geom = "text",
+        x = 1,
+        y = 46,
+        label = 'Annapurna I has the highest\nfatility ratio of the 8000ers\nthis is in part due to the steep south face\nwhich consists of a 3000m wall of rock',
+        family = "Courier New",
+        size = 2.5,
+        colour = grey(0.9))
 
+ggsave("2020/week39_Himalayan/AnnapurnaMassif.png", 
+       plot, 
+       height = 8, width = 13, 
+       units = "in", dpi = 600)
