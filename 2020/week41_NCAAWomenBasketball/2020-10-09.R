2a06cce78479d5c0b9dc2554f82d1708229b01b2
Tanya Strydom <tanya.strydom@icloud.com>
2020-10-06 11:59:47 +0200

    🖌 initial commit

---
+++ 2020/week41_NCAAWomenBasketball/2020-10-09.R
@@ -0,0 +1,18 @@
+#' ------------------------------------------------------------------#
+#' NCAA WOMENS BASKETBALL
+#'  - Data and README can be found at:
+#'  (https://github.com/rfordatascience/tidytuesday/tree/master/data/2020/2020-10-06)
+#' ------------------------------------------------------------------#
+
+### 0) Preamble ----
+### >> a) Dependencies ----
+library(tidytext)
+library(tidyverse)
+library(tidylog)
+
+#todays prompts
+options(prompt = c("\U1F3C0"),
+        continue = "\U1F3C6")
+
+### >> b) Data ----
+tournament <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-10-06/tournament.csv')
